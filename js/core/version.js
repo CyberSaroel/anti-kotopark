6477f140686c40c77ae8598a034039dbae7f4053
CyberSaroel <sarobagmanyan@gmail.com>
2026-08-03 16:58:23 +0400

    0.6.0

--- js/core/version.js
+++ js/core/version.js
@@ -1,4 +1,4 @@
-export const VERSION = "0.5.0";
+export const VERSION = "0.6.0";
 
 const STORAGE_KEY = "socio-cats:version";
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "0.5.0"; // это число будет менять tools/bumpVersion.js
+const VERSION = "0.6.0"; // это число будет менять tools/bumpVersion.js
 const CACHE = "royal-socio-cats-" + VERSION;
 
 // Ставим новую версию воркера сразу, не ждём закрытия вкладок

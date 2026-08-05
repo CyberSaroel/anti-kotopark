e2d2ecca69f0063b9595cfd7ba778ddd15450a0b
CyberSaroel <sarobagmanyan@gmail.com>
2026-08-05 17:58:08 +0400

    0.8.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "0.7.0"; // это число будет менять tools/bumpVersion.js
+const VERSION = "0.8.0"; // это число будет менять tools/bumpVersion.js
 const CACHE = "royal-socio-cats-" + VERSION;
 
 // Ставим новую версию воркера сразу, не ждём закрытия вкладок

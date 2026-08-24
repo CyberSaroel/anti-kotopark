9abab2f320315389c23e3e8e980b845cc22fe886
CyberSaroel <sarobagmanyan@gmail.com>
2026-08-25 00:06:38 +0400

    тест

--- temp_fix.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const filePath = 'js/screens/gameScreen.js';
-let content = fs.readFileSync(filePath, 'utf8');
-content = content.replace('🚀 Ракеты', '🐠 Ракеты');
-fs.writeFileSync(filePath, content);
-console.log('Fixed');

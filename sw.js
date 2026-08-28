75c7255f85708a1c3e023cf9891d94dce48137c8
CyberSaroel <sarobagmanyan@gmail.com>
2026-08-28 20:24:23 +0400

    фикс право на ошибку

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "0.38.0"; // это число будет менять tools/bumpVersion.js
+const VERSION = "0.39.0"; // это число будет менять tools/bumpVersion.js
 const CACHE = "anti-kotopark-" + VERSION;
 
 // Ставим новую версию воркера сразу, не ждём закрытия вкладок

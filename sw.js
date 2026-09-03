d95e2bc536a126c36ab9812ce48432928ea5fff5
CyberSaroel <sarobagmanyan@gmail.com>
2026-09-04 00:36:31 +0400

    фикс

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1.3.0"; // это число будет менять tools/bumpVersion.js
+const VERSION = "1.4.0"; // это число будет менять tools/bumpVersion.js
 const CACHE = "anti-kotopark-" + VERSION;
 
 // Ставим новую версию воркера сразу, не ждём закрытия вкладок
